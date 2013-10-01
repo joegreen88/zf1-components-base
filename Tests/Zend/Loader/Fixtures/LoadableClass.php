d4591a2234c2db3094b54c08bcdc4303f7bf4819
Joe Green <joe.green@smrtr.co.uk>
2013-10-01 15:45:57 +0100

    Fixed typo in comment

--- Tests/Zend/Loader/Fixtures/LoadableClass.php
+++ Tests/Zend/Loader/Fixtures/LoadableClass.php
@@ -7,7 +7,7 @@ namespace Tests\Zend\Loader\Fixtures;
  *
  * This class is loadable through composer because we have defined a PSR autoloader for Tests\Zend.
  * Zend_Loader::loadClass() would never find this on it's own.
- * But with out modifications to Zend_Loader::loadClass(), it should be able to find this class thanks to composer.
+ * But with our modifications to Zend_Loader::loadClass(), it should be able to find this class thanks to composer.
  */
 class LoadableClass
 {
